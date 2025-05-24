6310ef4e041dc805bc76dcb75290ae12f019c2e9
BreadOne <bbangwon.chung@gmail.com>
2025-05-24 17:05:40 +0900

    헤더 컴포넌트에서 랜덤 설명 추가

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,10 +1,17 @@
+const reactDesciptions = ["Fundmantal", "Crucial", "Core"];
+
+function getRandimInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
+  const description = reactDesciptions[getRandimInt(2)];
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {description} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
