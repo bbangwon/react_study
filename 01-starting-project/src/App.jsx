1aaff5ab9676c47497d0602ae3f89a969ca12945
bbangwon <bbangwon.chung@gmail.com>
2025-05-25 17:58:36 +0900

    상태 관리 및 예제 데이터 추가, 선택된 주제에 따른 동적 콘텐츠 표시 구현

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,12 +1,15 @@
-import { CORE_CONCEPTS } from "./data.js";
+import { useState } from "react";
+import { CORE_CONCEPTS, EXAMPLES } from "./data.js";
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState("components");
+
   function handleSelect(selectedButton) {
     // selectedButton => 'components' | 'jsx' | 'props' | 'state'
-    console.log(selectedButton);
+    setSelectedTopic(selectedButton);
   }
 
   return (
@@ -32,7 +35,13 @@ function App() {
             <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
             <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-          Dynamic Content
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
