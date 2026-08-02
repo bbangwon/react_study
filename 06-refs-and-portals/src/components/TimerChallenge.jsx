c02a7c2dd52dc677d6a5e9796820d6e4ea2d001a
BreadOne <bbangwon.chung@gmail.com>
2026-08-02 10:54:26 +0900

    "DOM 요소 연결"외 Refs(참조) 활용법

--- 06-refs-and-portals/src/components/TimerChallenge.jsx
+++ 06-refs-and-portals/src/components/TimerChallenge.jsx
@@ -1,11 +1,13 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function TimerChallenge({ title, targetTime }) {
+  const timer = useRef();
+
   const [timerStarted, setTimerStarted] = useState(false);
   const [timerExpired, setTimerExpired] = useState(false);
-  
-  function handleStart() {    
-    setTimeout(() => {
+
+  function handleStart() {
+    timer.current = setTimeout(() => {
       setTimerExpired(true);
     }, targetTime * 1000);
 
@@ -13,21 +15,23 @@ export default function TimerChallenge({ title, targetTime }) {
   }
 
   function handleStop() {
-    
+    clearTimeout(timer.current);
   }
-  
+
   return (
     <section className="challenge">
       <h2>{title}</h2>
-      {timerExpired && <p>You lost!</p>}      
+      {timerExpired && <p>You lost!</p>}
       <p className="challenge-time">
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <button onClick={handleStart}>{timerStarted ? "Stop" : "Start" } Challenge</button>
+        <button onClick={timerStarted ? handleStop : handleStart}>
+          {timerStarted ? "Stop" : "Start"} Challenge
+        </button>
       </p>
-      <p className={timerStarted ? 'active' : undefined}>
-        {timerStarted ? 'Time is running...' : 'Timer inactive' }
+      <p className={timerStarted ? "active" : undefined}>
+        {timerStarted ? "Time is running..." : "Timer inactive"}
       </p>
     </section>
   );
