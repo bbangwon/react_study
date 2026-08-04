5c4ee08d1e7dab49c385290b0afae4aafff6cf73
youngil <youngil@claion.co.kr>
2026-08-04 21:58:46 +0900

    모달 컴포넌트 추가하기

--- 06-refs-and-portals/src/components/TimerChallenge.jsx
+++ 06-refs-and-portals/src/components/TimerChallenge.jsx
@@ -1,4 +1,5 @@
 import { useState, useRef } from "react";
+import ResultModal from "./ResultModal";
 
 export default function TimerChallenge({ title, targetTime }) {
   const timer = useRef();
@@ -19,9 +20,10 @@ export default function TimerChallenge({ title, targetTime }) {
   }
 
   return (
+    <>
+    {timerExpired && <ResultModal targetTime={targetTime} result="lost" />}
     <section className="challenge">
       <h2>{title}</h2>
-      {timerExpired && <p>You lost!</p>}
       <p className="challenge-time">
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
@@ -34,5 +36,6 @@ export default function TimerChallenge({ title, targetTime }) {
         {timerStarted ? "Time is running..." : "Timer inactive"}
       </p>
     </section>
+    </>
   );
 }
